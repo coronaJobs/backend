63d8c7c247b218c48c4dcc026e927f58e11aa1d2
jiaranda <jiaranda@uc.cl>
2020-04-25 19:39:59 -0400

    :lock: user password encryption

--- src/models/user.js
+++ src/models/user.js
@@ -1,14 +1,38 @@
 'use strict'
+
+const bcrypt = require('bcryptjs');
+
+async function buildPasswordHash(instance) {
+  if (instance.changed('password')) {
+    const salt = await bcrypt.genSalt(Number(process.env.PASSWORD_SALT));
+    const hash = await bcrypt.hash(instance.password, salt);
+    instance.set('password', hash);
+  }
+}
+
 module.exports = (sequelize, DataTypes) => {
     const user = sequelize.define('user', {
       rut: DataTypes.INTEGER,
       name: DataTypes.STRING,
       mail: DataTypes.STRING,
       phone: DataTypes.STRING,
+      password: DataTypes.STRING,
       address: DataTypes.STRING,
       profilePicture: DataTypes.STRING,
       resumeUrl: DataTypes.STRING,
       active: {type: DataTypes.BOOLEAN, defaultValue: true},
     });
+
+    user.beforeUpdate(buildPasswordHash);
+    user.beforeCreate(buildPasswordHash);
+
+    user.prototype.checkPassword = function checkPassword(password) {
+      return bcrypt.compare(password, this.password);
+    };
+
+    // user.associate = models => {
+    //   user.belongsTo(models.someModel);
+    // };
+
     return user;
   };
