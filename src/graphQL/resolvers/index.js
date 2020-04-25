79e470ef3d73f2a7573addacd1c81aacf5aaabc3
vaguila95 <vlaguila@uc.cl>
2020-04-25 19:53:49 -0400

    :fire: deletion of usless template files

--- src/graphQL/resolvers/index.js
+++ src/graphQL/resolvers/index.js
@@ -1,4 +1,3 @@
-const message = require('./message');
 const user = require('./user');
 
 // Import all the resolvers
@@ -11,21 +10,18 @@ const resolvers = {
 // Assign the queries to the specific object
 Object.assign(
   resolvers.Query,
-  message.Query,
   user.Query,
 );
 
 // Assign the mutations to the specific object
 Object.assign(
   resolvers.Mutation,
-  message.Mutation,
   user.Mutation,
 );
 
 // Assign the subscriptions to the specific object
 // Object.assign(
 //   resolvers.Subscription,
-//   message.Subscription,
 // );
 
 module.exports = resolvers;
