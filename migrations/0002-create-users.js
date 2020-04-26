87d657c2a28da0f2ed835802a36547b0c7bff84b
jiaranda <jiaranda@uc.cl>
2020-04-25 20:56:55 -0400

    :recycle: add password to user

--- migrations/0002-create-users.js
+++ migrations/0002-create-users.js
@@ -21,10 +21,6 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      password: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
       phone: {
         type: Sequelize.STRING,
         allowNull: false,
