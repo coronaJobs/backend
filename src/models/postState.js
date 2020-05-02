99cf27a61c7331434e3bb3480fa63629faccf7f0
sgcarreno <secarro@hotmail.com>
2020-05-02 11:59:42 -0400

    :sparkles: postState migration and model

---
+++ src/models/postState.js
@@ -0,0 +1,14 @@
+'use strict'
+module.exports = (sequelize, DataTypes) => {
+    const postState = sequelize.define('postState', {
+      name: DataTypes.STRING,
+      description: DataTypes.STRING,
+      active: {type: DataTypes.BOOLEAN, defaultValue: true},
+    });
+
+    postState.associate = (models) => {
+        postState.hasMany(models.post)
+      }
+
+    return postState;
+  };
