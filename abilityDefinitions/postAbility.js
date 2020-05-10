94c5664ff82e17b18ed64816b0c44031ac1ba833
jiaranda <jiaranda@uc.cl>
2020-05-10 11:33:29 -0400

    :bug: added validation to postAbility

--- abilityDefinitions/postAbility.js
+++ abilityDefinitions/postAbility.js
@@ -3,6 +3,9 @@ const { db } = require('./../src/models')
 const postAbility = (ability, user) => {
     if (user) {
         ability.createAbility(db.post, 'update', async ({post}) => {
+            if (!post) {
+                return false
+            }
             const owner = await post.getOwner()
             return owner.id == user.id
         })
