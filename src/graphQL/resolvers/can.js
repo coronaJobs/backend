5e0c1dec9d6c1ced0448377ec3dfa2418c45dc5b
jiaranda <jiaranda@uc.cl>
2020-05-10 12:41:48 -0400

    :sparkles: added validations

--- src/graphQL/resolvers/can.js
+++ src/graphQL/resolvers/can.js
@@ -1,4 +1,8 @@
 const { db } = require('../../models');
+const validator = require('validator');
+const {
+    UserInputError
+} = require('apollo-server')
 
 
 module.exports = {
@@ -7,25 +11,55 @@ module.exports = {
   Query: {
     can: async (_, params, ctx) => {
       const {entityName, action, filters} = params
-    
-      const entity = db[entityName.toLowerCase()]
+
+      // validations
+
+      if (!db[entityName]) throw new UserInputError('entityName: must be from a valid entity') 
 
       const filter = {}
 
       if (filters) {
+        
+        // filters valdiations
+        for (const canFilter of filters) {
+            if (typeof canFilter.name == "undefined") throw new UserInputError('filters: if you pass the filters parameter, name cannot be undefined')
+            if (typeof canFilter.id == "undefined") throw new UserInputError('filters: if you pass the filters parameter, id cannot be undefined')
+            if (!(typeof canFilter.name == "string")) throw new UserInputError('filters: name must be a String')
+            if (!Number.isInteger(canFilter.id)) throw new UserInputError('filters: id must be an Integer')
+            if (!db[canFilter.name]) throw new UserInputError('filters: name must be from a valid entity')    
+        }
+
         for (const canFilter of filters) {
             const instance =  await db[canFilter.name.toLowerCase()].findByPk(canFilter.id)
-    
             if (!instance) {
                 return false
-            }
-    
+            }            
             filter[canFilter.name.toLowerCase()] = instance
           } 
       }
-           
 
-      return await ctx.ability.can(entity, action.toLowerCase(), filter)
+      const entity = db[entityName.toLowerCase()]
+
+      try {
+
+        const result = await ctx.ability.can(entity, action.toLowerCase(), filter)
+
+        return result
+
+      } catch (error) {
+          
+          if (!error.extensions) throw error
+
+          if (error.extensions.code == "BAD_USER_INPUT") {
+              throw new UserInputError('filters: '+ error.message)
+          } else {
+              throw error
+          }
+      }
+
+      
+
+      
     },
   },
 };
