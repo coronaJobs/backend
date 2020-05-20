e9275d847538c056e157d00638d26d7b6cabad90
jiaranda <jiaranda@uc.cl>
2020-05-20 16:06:57 -0400

    :sparkles: post filtering ready

--- src/graphQL/resolvers/post.js
+++ src/graphQL/resolvers/post.js
@@ -1,11 +1,56 @@
 const { db } = require('../../models');
+const { validatePostSearchParameters } = require('../../validations');
+const { Op } = require("sequelize");
+const { DateTime } = require("luxon")
 
 module.exports = {
   Subscription: {},
 
   Query: {
-    getAllPosts: async (_, params, ctx) => {
-        return await db.post.findAll(params)
+    getAllPosts: async (_, params, ctx) => {        
+        validatePostSearchParameters(params)
+        const {text, fromDate, toDate, ownerId, fromApplicantLimit, toApplicantLimit} = params
+
+        const filter = { where: {}}
+
+        if (text) {
+          Object.assign(
+            filter.where,
+            { [Op.or]: {
+              name: { [Op.iLike]: '%' + text + '%' },
+              description: { [Op.iLike]: '%' + text + '%' }
+              }
+            }
+          )
+        }
+
+
+        if (fromDate && toDate) {
+          filter.where.createdAt = { [Op.between]: [DateTime.fromISO(fromDate, {zone: 'UTC'}).toISO(), DateTime.fromISO(toDate, {zone: 'UTC'}).toISO()] }
+        } else if (fromDate) {
+          filter.where.createdAt = { [Op.gte]: DateTime.fromISO(fromDate, {zone: 'UTC'}).toISO() }
+        } else if (toDate) {
+          filter.where.createdAt = { [Op.lte]: DateTime.fromISO(toDate, {zone: 'UTC'}).toISO() }
+        }
+
+
+        if (ownerId) {
+          filter.where.ownerId = ownerId
+        }
+
+        
+        if (fromApplicantLimit && toApplicantLimit) {
+          filter.where.applicantLimit = { [Op.between]: [fromApplicantLimit, toApplicantLimit] }
+        } else if (fromApplicantLimit) {
+          filter.where.applicantLimit = { [Op.gte]: fromApplicantLimit }
+        } else if (toApplicantLimit) {
+          filter.where.applicantLimit = { [Op.lte]: toApplicantLimit }
+        }
+        
+        console.log(filter);
+        
+
+        return await db.post.findAll(filter)
     },
   },
 
