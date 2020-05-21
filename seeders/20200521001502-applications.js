2c3ac2658b34f57be7742106c45cb437386ff18e
sgcarreno <secarro@hotmail.com>
2020-05-20 20:55:05 -0400

    :seedling: applications seed file

---
+++ seeders/20200521001502-applications.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: queryInterface => {
+    return queryInterface.bulkInsert(
+      'applications',
+      [
+        {
+          applicantId: 1,
+          offerId: 1,
+          createdAt: '2020-05-20 20:30:46.847+00',
+          updatedAt: '2020-05-20 20:30:46.847+00',
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async queryInterface => {
+    await queryInterface.bulkDelete('applications', null, {});
+    await queryInterface.sequelize.query(
+      'alter sequence applications_id_seq restart with 1;'
+    );
+  },
+};
