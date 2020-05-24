5947b9cf7f4795ac98dd34c7eaa6c722ffad2b67
jiaranda <jiaranda@uc.cl>
2020-05-24 16:10:51 -0400

    :construction: added commune to posts database

---
+++ seeders/00000000000000-communes.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const rawCommunes = require("./../staticData/communes");
+const communes = [];
+rawCommunes.forEach((com) => {
+  communes.push({
+    createdAt: "2020-04-27 16:30:46.847+00",
+    updatedAt: "2020-04-27 16:30:46.847+00",
+    name: com,
+  });
+});
+
+console.log(communes);
+
+module.exports = {
+  up: (queryInterface) => {
+    return queryInterface.bulkInsert("communes", communes, {});
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.bulkDelete("communes", null, {});
+    await queryInterface.sequelize.query(
+      "alter sequence communes_id_seq restart with 1;"
+    );
+  },
+};
