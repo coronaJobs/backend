9597d678af4ef058f40b252979a819e5c3733d32
cnscheihing <cnscheihing@uc.cl>
2020-06-21 17:43:05 -0400

    :art: refactor: console log deleted

--- src/graphQL/resolvers/user.js
+++ src/graphQL/resolvers/user.js
@@ -197,7 +197,6 @@ module.exports = {
     rating: async (user) => {
       let globalRating = 0;
       const roleName = getRoleNames(user)[user.roleId];
-      // console.log('EL ROLE', roleName);
       const employments = await getEmploymentsByRole(user);
       employments.forEach((employment) => {
         globalRating += employment[roleName.ratingName];
